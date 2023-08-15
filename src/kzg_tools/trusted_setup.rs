cf9e18727028d08a76ecd998544f8710e979b131
SerxaS <serhas@gmail.com>
2023-08-15 19:53:51 +0300

    trusted setup added

--- src/kzg_tools/trusted_setup.rs
+++ src/kzg_tools/trusted_setup.rs
@@ -1,1 +1,21 @@
+use super::fft::pow;
+use halo2::{
+    arithmetic::Field,
+    halo2curves::bn256::{Fr, G1, G2},
+};
+use rand::thread_rng;
+use std::ops::Mul;
 
+fn trusted_setup(p: Vec<Fr>) -> (Vec<G1>, G2) {
+    let rng = thread_rng();
+    let trusted_s = Fr::random(rng.clone());
+    let mut trusted_p = Vec::new();
+    let s_g2 = G2::generator().mul(trusted_s);
+
+    for i in 0..p.len() {
+        let s_g1 = G1::generator().mul(pow(trusted_s, i.try_into().unwrap()).evaluation);
+        trusted_p.push(s_g1);
+    }
+
+    (trusted_p, s_g2)
+}
